f573a49d1f46c5fd2d629ea385a3ec374006f92e
Janaka Madusanka <rmjmrathnayaka@std.appsc.sab.ac.lk>
2025-03-11 22:23:59 +0530

    User Login controller function done

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -27,7 +27,35 @@ export const register = async (req,res)=>{
             sameSite:process.env.NODE_ENV === 'production' ? 'none': 'strict',
             maxAge:7*24*60*60*1000
         });
+        return res.json({success:true, message:"User created successfully"});
 
+    }catch(error){
+        res.json({success:false, message:error.message});
+    }
+}
+
+export const login = async (req,res)=>{
+    const {email,password} = req.body;
+    if(!email || !password){
+        return res.json({success:false, message:"Email and Password are required"});
+    }
+    try{
+        const user = await userModel.findOne({email});
+        if(!user){
+            return res.json({success:false, message:"Invalid Email"});
+        }
+        const isPasswordCorrect = await bcrypt.compare(password,user.password);
+        if(!isPasswordCorrect){
+            return res.json({success:false, message:"Invalid Password"});
+        }
+        const token =jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:'7d'});
+        res.cookie('token',token,{
+            httpOnly:true,
+            secure:process.env.NODE_ENV === 'production',
+            sameSite:process.env.NODE_ENV === 'production' ? 'none': 'strict',
+            maxAge:7*24*60*60*1000
+        });
+        return res.json({success:true, message:"Login Success"});
     }catch(error){
         res.json({success:false, message:error.message});
     }
