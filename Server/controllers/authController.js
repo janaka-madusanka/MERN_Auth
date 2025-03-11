4fffb785315ab1e0b41a8e1cf26d4d4b89e634c9
Janaka Madusanka <rmjmrathnayaka@std.appsc.sab.ac.lk>
2025-03-11 23:02:18 +0530

    User Logged out controller function done.

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -55,7 +55,23 @@ export const login = async (req,res)=>{
             sameSite:process.env.NODE_ENV === 'production' ? 'none': 'strict',
             maxAge:7*24*60*60*1000
         });
+
         return res.json({success:true, message:"Login Success"});
+
+    }catch(error){
+        res.json({success:false, message:error.message});
+    }
+}
+
+
+export const logout = async (req,res)=>{
+    try{
+        res.clearCookie('token',{
+        httpOnly:true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite:process.env.NODE_ENV === 'production' ? 'none': 'strict',
+        })
+        return res.json({success:true, message:"Logged out successfully"});
     }catch(error){
         res.json({success:false, message:error.message});
     }
