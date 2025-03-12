ebf7a0bf5aafa69f803b925b505dde2bae579cf9
Janaka Madusanka <rmjmrathnayaka@std.appsc.sab.ac.lk>
2025-03-13 01:33:46 +0530

    Get user data controller function done and tested.

--- Server/server.js
+++ Server/server.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 
 import connectDB from './config/mongodb.js';
 import authRouter from './routes/authRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 
 
@@ -19,4 +20,5 @@ app.use(cors({credentials:true}));
 //API endpoints
 app.get('/',(req,res)=>res.send('API is running... nodemon is added'));
 app.use('/api/auth',authRouter);
+app.use('/api/user',userRouter);
 app.listen(port,()=>console.log(`Server is running on port ${port}`));
